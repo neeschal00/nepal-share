87dab117d6b86f343cf62aa2bd894205ab844143
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2023-08-09 11:45:26 +0545

    Update getshareprice.py

--- getshareprice.py
+++ getshareprice.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     mongo_url = os.environ['MONGOURL']
     connect(mongo_url,alias='my-app')
     
-    my_shares = ['SGIC','NIFRA','SRLI','GVL','MBJC','RULB','USHEC','TAMOR','PPL',"ILI"]
+    my_shares = ['SGIC','NIFRA','SRLI','GVL','MBJC','ALBSL','USHEC','TAMOR','PPL',"ILI"]
 
     for symbol in my_shares:
         logging.info("processing this page "+ symbol)
